894dee0cf7ec004053932b07156c60fe56fd0525
pg <pgforworks@gmail.com>
2015-12-09 20:57:51 +0300

    finish sign in

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,6 +15,13 @@ RSpec.describe "UserPages", type: :request do
       it "should not create a user" do
         expect { click_button submit }.not_to change  {User.count}
       end
+
+      describe "after submission" do
+        before {click_button submit}
+        it {should have_title('Sing up')}
+        it {should have_content('error')}
+      end
+
     end
 
     describe "with valid information" do
@@ -26,6 +33,13 @@ RSpec.describe "UserPages", type: :request do
       end
       it "should create a user" do
         expect {click_button submit}.to change {User.count}.by(1)
+      end
+
+      describe "after saving the user" do
+        before {click_button submit}
+        let(:user){User.find_by(email: "kovalnkosa_1@mail.ru")}
+        it {should have_title(user.name)}
+        it {should have_selector('div.alert.alert-success', text: 'Добро')}
       end
     end
 
